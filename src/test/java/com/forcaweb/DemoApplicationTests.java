ea0ed4ef412851289cc0465e07124001fb06d357
forcaweb <forcawebsuporte@gmail.com>
2024-08-03 22:26:06 -0300

    first commit

---
+++ src/test/java/com/forcaweb/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forcaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
