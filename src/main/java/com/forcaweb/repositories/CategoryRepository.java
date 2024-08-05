111d1dafae23a3d3a55152aed5f1fd9b205096fe
forcaweb <forcawebsuporte@gmail.com>
2024-08-05 20:28:58 -0300

    at0508202420

---
+++ src/main/java/com/forcaweb/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.forcaweb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.forcaweb.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
